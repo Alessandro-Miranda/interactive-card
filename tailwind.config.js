27d51ebe030be45a24c9085554b97f36872d618f
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2022-08-09 17:15:05 -0300

    iniciado a implementação mobile

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    colors: {
+      red: 'hsl(0, 100%, 66%)',
+      white: 'hsl(0, 0%, 100%)',
+      'light-grayish-violet': 'hsl(270, 3%, 87%)',
+      'dark-grayish-violet': 'hsl(279, 6%, 55%)',
+      'very-dark-violet': 'hsl(278, 68%, 11%)'
+    }
+  },
+  plugins: [],
+}
