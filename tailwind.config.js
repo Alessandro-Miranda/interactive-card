20d88639850cec900cecbf25968ab6b2cbe27648
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2022-08-11 12:03:29 -0300

    incluido o azul utilizado no footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,8 @@ module.exports = {
       white: 'hsl(0, 0%, 100%)',
       'light-grayish-violet': 'hsl(270, 3%, 87%)',
       'dark-grayish-violet': 'hsl(279, 6%, 55%)',
-      'very-dark-violet': 'hsl(278, 68%, 11%)'
+      'very-dark-violet': 'hsl(278, 68%, 11%)',
+      'mentor-blue': 'hsl(249, 99%, 64%)'
     }
   },
   plugins: [],
